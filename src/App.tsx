761c656a838b51d7a182604349405ec5776b5c23
sarataber <saraaber3732@gmail.com>
2026-03-10 11:55:52 +0200

    שינוי תוכן הדף הראשי

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,14 @@ function App() {
   const [file, setFile] = useState<File | null>(null);
   const [documents, setDocuments] = useState<Documents[]>([]);
   const [loading, setLoading] = useState(false);
+  const [loadingDocs, setLoadingDocs] = useState(false);
 
   const fetchDocuments = async () => {
+    setLoadingDocs(true);
     const res = await fetch("/documents");
     const data = await res.json();
     setDocuments(data);
+    setLoadingDocs(false);
   };
 
   useEffect(() => {
@@ -35,6 +38,10 @@ function App() {
     });
 
     const { uploadUrl, key } = await presignRes.json();
+    console.log(uploadUrl, key);
+
+
+
 
     // 2️⃣ העלאה ישירה ל-S3
     await fetch(uploadUrl, {
@@ -44,6 +51,8 @@ function App() {
       },
       body: file,
     });
+
+
 
     // 3️⃣ שמירת metadata ב-DB
     await fetch("/documents", {
@@ -61,11 +70,10 @@ function App() {
     await fetchDocuments();
     setLoading(false);
   };
-
   return (
     <div style={{ padding: 40 }}>
       <h1>DocBox</h1>
-      <h2 style={{backgroundColor:"green"}}>פורים שמח!</h2>
+      <h2 style={{ backgroundColor: "lightgray" }}> יום נעים, אפשר להתחיל לעלות מסכמכים</h2>
       <input
         type="file"
         onChange={(e) => setFile(e.target.files?.[0] || null)}
@@ -78,9 +86,32 @@ function App() {
       <hr />
 
       <h2>Documents</h2>
+      {loadingDocs ? "Loading..." : null}
       <ul>
         {documents.map((doc) => (
-          <li key={doc.id}>{doc.title}</li>
+          <li key={doc.id}>
+            {doc.title}
+
+            <button
+              onClick={async () => {
+                const res = await fetch(`/documents/${doc.id}/download`);
+                const data = await res.json();
+                window.open(data.url, "_blank");
+              }}
+            >
+              Download
+            </button>
+            <button
+              onClick={async () => {
+                await fetch(`/documents/${doc.id}`, {
+                  method: "DELETE",
+                });
+                fetchDocuments();
+              }}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
